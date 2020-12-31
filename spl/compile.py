fa5679cd57da85d67a3fe87d25448b031caafc36
shrn01 <sharanjs1999@gmail.com>
2020-12-31 19:12:57 +0530

    Change of plans, stage 27, has some bugs

--- spl/compile.py
+++ spl/compile.py
@@ -6,11 +6,4 @@ for i in l:
         print('./spl '+ os.getcwd() +'/spl_progs/'+ i)
         os.system('./spl '+ os.getcwd() +'/spl_progs/'+ i)
 
-
-l = os.listdir(os.getcwd() + '/spl_progs/s15')
-for i in l:
-    if '.spl' in i:
-        print('./spl '+ os.getcwd() +'/spl_progs/s15/'+ i)
-        os.system('./spl '+ os.getcwd() +'/spl_progs/s15/'+ i)
-
 print("finish")
